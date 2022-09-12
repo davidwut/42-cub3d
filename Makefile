88f2ad7c465afaee3780e79dd07f4724a8585267
David <wut.dav@gmail.com>
2022-09-12 17:13:50 +0200

    needs investigating

--- Makefile
+++ Makefile
@@ -1,20 +1,20 @@
-NAME			:= cub3d
+NAME		:= cub3d
 
-SRC_DIR			:= src/
-SRC				:= main.c
+SRC_DIR		:= src/
+SRC			:= main.c
 
-SRCS			:= $(addprefix $(SRC_DIR), $(SRC))
-OBJS			:= $(SRCS:%.c=%.o)
-OBJS_DIR		:= objs
+SRCS		:= $(addprefix $(SRC_DIR), $(SRC))
+OBJS		:= $(SRCS:%.c=%.o)
+OBJS_DIR	:= objs
 
-MLIB			:= libmlx_Linux.a
-MLX_DIR			:= mlx_linux
-MLX				:= $(MLX_DIR)/$(MLIB)
+MLIB		:= libmlx_Linux.a
+MLX_DIR		:= mlx_linux
+MLX			:= $(MLX_DIR)/$(MLIB)
 
-CC				:= gcc
-CFLAGS			:= -Wall -Wextra -Werror
+CC			:= gcc
+CFLAGS		:= -Wall -Wextra -Werror
 
-RM				:= rm -rf
+RM			:= rm -rf
 
 RED			:=\033[0;31m
 PURPLE		:=\033[0;35m
--- src/main.c
+++ src/main.c
@@ -6,19 +6,53 @@
 /*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 12:02:24 by dwuthric          #+#    #+#             */
-/*   Updated: 2022/09/12 16:09:18 by dwuthric         ###   ########.fr       */
+/*   Updated: 2022/09/12 17:13:23 by dwuthric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// Color is ARGB
+
 #include <stdio.h>
 #include <mlx.h>
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+void	px_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void	draw_rectangle(t_data *data, int x, int y, int width, int height, int color)
+{
+	for (int dx = 0; dx < width; dx++)
+	{
+		for(int dy = 0; dy < height; dy++)
+		{
+			px_put(data, x + dx, y + dy, color);
+		}
+	}
+}
 
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	t_data	img;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 900, 500, "Cub3d");
+	mlx_win = mlx_new_window(mlx, 1200, 900, "Cub3d");
+	img.img = mlx_new_image(mlx, 1200, 900);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	draw_rectangle(&img, 100, 100, 100, 100, 0x00ff0000);
 	mlx_loop(mlx);
 }

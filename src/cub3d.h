fe1b6809ab4a72a4191507f7d5f32092c9b22c3b
David <wut.dav@gmail.com>
2022-09-13 16:46:13 +0200

    quick push before leaving

---
+++ src/cub3d.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dwuthric <dwuthric@student42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/13 15:19:10 by dwuthric          #+#    #+#             */
+/*   Updated: 2022/09/13 16:45:37 by dwuthric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include <mlx.h>
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+typedef struct	s_env {
+	int	screen
+}	t_env;
+
+#endif
